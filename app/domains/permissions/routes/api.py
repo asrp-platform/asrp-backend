5f78b1c200e11799a15e6abf02418ddc8dfebe93
Maksim <maxevg72@gmail.com>
2026-01-27 23:17:36 +0300

    chore: Added summary to current user permissions endpoint

--- app/domains/permissions/routes/api.py
+++ app/domains/permissions/routes/api.py
@@ -6,6 +6,9 @@ from app.domains.shared.deps import AdminPermissionsDep
 router = APIRouter(prefix="/permissions", tags=["Permissions"])
 
 
-@router.get("/current-user-permissions/")
+@router.get(
+    "/current-user-permissions/",
+    summary="List of permissions for the authenticated user",
+)
 async def get_current_user_permissions(permissions: AdminPermissionsDep) -> list[PermissionSchema]:
     return [PermissionSchema.from_orm(permission) for permission in permissions]
