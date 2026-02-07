a8b38121d01e04ffab33b5ac908b29118e464549
Alexandr <mr.borovkov.prm@gmail.com>
2026-02-08 01:36:22 +0500

    Feature: Add model users fields (#10)
    * feature: Added fields to the user's model and schema.
    
    * feature: Added fields to the registration scheme
    
    * feature: Conducted migrations
    
    * feature: Updated the fixtures in the tests
    
    * refactor: In the users domain, I have separated models and schemas
    
    * fix: combining parallel migrations
    
    * linter edits

--- alembic/versions/ae9dd6e12821_deleted_sponsorship_requests.py
+++ alembic/versions/ae9dd6e12821_deleted_sponsorship_requests.py
@@ -14,8 +14,8 @@ from sqlalchemy.dialects import postgresql
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "002"
-down_revision: Union[str, None] = "001"
+revision = "ae9dd6e12821"
+down_revision = "2f846565bf54"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
