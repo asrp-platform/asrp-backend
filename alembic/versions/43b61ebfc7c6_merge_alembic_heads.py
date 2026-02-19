13187b5dc907fe7f35d4c931c662147638bdb081
LizVin19 <e192000@gmail.com>
2026-02-19 16:35:53 +0300

    chore: fix invalid noqa & format alembic merge migration

--- alembic/versions/43b61ebfc7c6_merge_alembic_heads.py
+++ alembic/versions/43b61ebfc7c6_merge_alembic_heads.py
@@ -5,15 +5,12 @@ Revises: 78a6c82b5bd6, b9dc4ec67c04
 Create Date: 2026-02-19 16:18:38.598198
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
-revision: str = '43b61ebfc7c6'
-down_revision: Union[str, None] = ('78a6c82b5bd6', 'b9dc4ec67c04')
+revision: str = "43b61ebfc7c6"
+down_revision: Union[str, None] = ("78a6c82b5bd6", "b9dc4ec67c04")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
