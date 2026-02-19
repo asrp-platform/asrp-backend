d3704c76ca140247a7724e0a6bb22470cd9a317d
LizVin19 <e192000@gmail.com>
2026-02-19 16:21:57 +0300

    chore: merge alembic heads

---
+++ alembic/versions/43b61ebfc7c6_merge_alembic_heads.py
@@ -0,0 +1,26 @@
+"""merge alembic heads
+
+Revision ID: 43b61ebfc7c6
+Revises: 78a6c82b5bd6, b9dc4ec67c04
+Create Date: 2026-02-19 16:18:38.598198
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '43b61ebfc7c6'
+down_revision: Union[str, None] = ('78a6c82b5bd6', 'b9dc4ec67c04')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
