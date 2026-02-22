8bd90fa64c7827118aa48a8f8966f4c0c3484468
Alexandr Borovkov <mr.borovkov.prm@gmail.com>
2026-02-22 20:31:06 +0500

    fix: remove duplicate table creation (fixing the linter)

--- alembic/versions/0f7e4c431a00_merge_two_heads_into_one.py
+++ alembic/versions/0f7e4c431a00_merge_two_heads_into_one.py
@@ -7,10 +7,6 @@ Create Date: 2026-02-22 19:54:49.835451
 """
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
 revision: str = '0f7e4c431a00'
 down_revision: Union[str, None] = ('43b61ebfc7c6', 'f40e24e4429d')
