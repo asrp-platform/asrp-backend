1822cbcc9b8b8229df8b09130a84c26831e0e126
Nikita Ayzikov <ayzikov070@mail.ru>
2026-02-23 14:57:08 +0300

    style: organize imports

--- alembic/versions/207ea20e433d_merge_with_develop.py
+++ alembic/versions/207ea20e433d_merge_with_develop.py
@@ -7,10 +7,6 @@ Create Date: 2026-02-23 14:50:04.870422
 """
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
 revision: str = '207ea20e433d'
 down_revision: Union[str, None] = ('43b61ebfc7c6', 'f9780a34a084')
