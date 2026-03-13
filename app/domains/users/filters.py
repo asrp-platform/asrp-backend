ecdf6157dc68a67dcce6ba5a3b52abe3ca8b4763
Maksim <maxevg72@gmail.com>
2026-03-13 22:40:42 +0300

    feat: Added filters, sorting and pagination for name change requests list endpoint

--- app/domains/users/filters.py
+++ app/domains/users/filters.py
@@ -3,6 +3,8 @@ from typing import Annotated
 from fastapi import Query
 from pydantic import BaseModel
 
+from app.domains.users.models import NameChangeRequestStatusEnum
+
 
 class UsersFilter(BaseModel):
     pending: Annotated[bool | None, Query(description="Pending filter")] = None
@@ -11,3 +13,7 @@ class UsersFilter(BaseModel):
     email__startswith: Annotated[str | None, Query(description="Email filter")] = None
     firstname__startswith: Annotated[str | None, Query(description="Firstname startswith")] = None
     lastname__startswith: Annotated[str | None, Query(description="Lastname startswith")] = None
+
+
+class NameChangeRequestsFilters(BaseModel):
+    status: Annotated[NameChangeRequestStatusEnum | None, Query(description="Status filter")] = None
