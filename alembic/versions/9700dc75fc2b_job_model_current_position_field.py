f64536f9e838791fcd1f4128c49ac58abad0ad9f
ayzikov <ayzikov070@mail.ru>
2026-03-24 14:58:28 +0300

    fix: job, current_position down_revision migration

--- alembic/versions/9700dc75fc2b_job_model_current_position_field.py
+++ alembic/versions/9700dc75fc2b_job_model_current_position_field.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '9700dc75fc2b'
-down_revision: Union[str, None] = '1901826ca4d1'
+down_revision: Union[str, None] = "046b3ddb4455"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
