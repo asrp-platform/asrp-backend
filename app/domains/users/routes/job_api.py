7261c3988290f8ced61adeab489f3c5e5df6bb6c
ayzikov <ayzikov070@mail.ru>
2026-03-31 18:57:01 +0300

    refactor: api error handling

--- app/domains/users/routes/job_api.py
+++ app/domains/users/routes/job_api.py
@@ -1,12 +1,9 @@
 from fastapi import APIRouter
 from fastapi_exception_responses import Responses
 
-from app.core.common.exceptions import NotResourceOwnerError
 from app.domains.shared.deps import CurrentUserDep
 from app.domains.users.exceptions import (
-    JobNotFoundError,
     ProfessionalExperienceCurrentPositionExistsError,
-    UserNotFoundError,
 )
 from app.domains.users.schemas import (
     JobCreateSchema,
@@ -34,11 +31,8 @@ async def get_user_jobs(
     user_id: int,
     service: JobServiceDep,
 ) -> list[JobViewSchema]:
-    try:
-        user_jobs = await service.get_by_user_id(user_id)
-        return [JobViewSchema.model_validate(job) for job in user_jobs]
-    except UserNotFoundError:
-        raise GetUserJobsResponses.USER_NOT_FOUND
+    user_jobs = await service.get_by_user_id(user_id)
+    return [JobViewSchema.model_validate(job) for job in user_jobs]
 
 
 class GetSingleUserJobResponses(GetUserJobsResponses):
@@ -55,18 +49,11 @@ async def get_single_user_job(
     job_id: int,
     service: JobServiceDep,
 ) -> JobViewSchema:
-    try:
-        user_job = await service.get_user_job_by_id(
-            user_id=user_id,
-            job_id=job_id,
-        )
-        return JobViewSchema.model_validate(user_job)
-
-    except UserNotFoundError:
-        raise GetSingleUserJobResponses.USER_NOT_FOUND
-
-    except JobNotFoundError:
-        raise GetSingleUserJobResponses.JOB_NOT_FOUND
+    user_job = await service.get_user_job_by_id(
+        user_id=user_id,
+        job_id=job_id,
+    )
+    return JobViewSchema.model_validate(user_job)
 
 
 class CreateUserJobResponses(GetUserJobsResponses):
@@ -93,12 +80,6 @@ async def create_job_for_user(
             **job_creation_data.model_dump(),
         )
         return JobViewSchema.model_validate(user_job)
-
-    except NotResourceOwnerError:
-        raise CreateUserJobResponses.NOT_RESOURCE_OWNER
-
-    except UserNotFoundError:
-        raise CreateUserJobResponses.USER_NOT_FOUND
 
     except ProfessionalExperienceCurrentPositionExistsError:
         raise CreateUserJobResponses.PROFESSIONAL_EXPERIENCE_CURRENT_POSITION_EXISTS
@@ -129,12 +110,6 @@ async def update_user_job(
         )
         return JobViewSchema.model_validate(user_job)
 
-    except NotResourceOwnerError:
-        raise UpdateJobResponses.NOT_RESOURCE_OWNER
-
-    except UserNotFoundError:
-        raise UpdateJobResponses.USER_NOT_FOUND
-
     except ProfessionalExperienceCurrentPositionExistsError:
         raise UpdateJobResponses.PROFESSIONAL_EXPERIENCE_CURRENT_POSITION_EXISTS
 
@@ -157,18 +132,8 @@ async def delete_user_job(
     current_user: CurrentUserDep,
     service: JobServiceDep,
 ) -> int:
-    try:
-        return await service.delete_user_job(
-            user_id,
-            current_user.id,
-            job_id,
-        )
-
-    except NotResourceOwnerError:
-        raise DeleteJobResponses.NOT_RESOURCE_OWNER
-
-    except UserNotFoundError:
-        raise DeleteJobResponses.USER_NOT_FOUND
-
-    except JobNotFoundError:
-        raise DeleteJobResponses.JOB_NOT_FOUND
+    return await service.delete_user_job(
+        user_id,
+        current_user.id,
+        job_id,
+    )
