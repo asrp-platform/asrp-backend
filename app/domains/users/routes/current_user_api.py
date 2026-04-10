5d53841d540811bc1de1aedbb26348275712d593
Maksim <maxevg72@gmail.com>
2026-04-10 15:31:58 +0300

    feat: Created current user payments

--- app/domains/users/routes/current_user_api.py
+++ app/domains/users/routes/current_user_api.py
@@ -1,9 +1,11 @@
 from typing import Annotated
 
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, Depends, File, UploadFile
 from fastapi_exception_responses import Responses
 
 from app.core.common.exceptions import NotResourceOwnerError
+from app.core.common.request_params import OrderingParamsDep, PaginationParamsDep
+from app.core.common.responses import PaginatedResponse
 from app.domains.feedback.exceptions import FeedbackAdditionalInfoAlreadyExistsError
 from app.domains.memberships.exceptions import MembershipRequestAlreadyExistsError
 from app.domains.memberships.schemas import (
@@ -16,6 +18,8 @@ from app.domains.memberships.use_cases.update_user_membership_mock import (
     UpdateUserMembershipMockRequest,
     UpdateUserMembershipMockUseCaseDep,
 )
+from app.domains.payments.filters import PaymentsFilter
+from app.domains.payments.schemas import PaymentReadSchema
 from app.domains.shared.deps import CurrentUserDep, CurrentUserMembershipDep
 from app.domains.users.exceptions import (
     InvalidPasswordError,
@@ -38,6 +42,7 @@ from app.domains.users.use_cases.delete_current_user_avatar import (
     DeleteCurrentUserAvatarRequest,
     DeleteCurrentUserAvatarUseCaseDep,
 )
+from app.domains.users.use_cases.retrieve_current_user_payments import RetrieveCurrentUserPaymentsUseCaseDep
 from app.domains.users.use_cases.set_current_user_avatar import (
     UploadCurrentUserAvatarRequest,
     UploadCurrentUserAvatarUseCaseDep,
@@ -224,6 +229,29 @@ async def create_membership_request(
         raise MembershipCreateResponses.MEMBERSHIP_ALREADY_EXISTS
     except FeedbackAdditionalInfoAlreadyExistsError:
         raise MembershipCreateResponses.FEEDBACK_ADDITIONAL_INFO_ALREADY_EXISTS
+
+
+@router.get("/payments")
+async def get_current_user_payments(
+    current_user: CurrentUserDep,
+    use_case: RetrieveCurrentUserPaymentsUseCaseDep,
+    params: PaginationParamsDep,
+    ordering: OrderingParamsDep = None,
+    filters: Annotated[PaymentsFilter, Depends()] = None,
+) -> PaginatedResponse[PaymentReadSchema]:
+    payments, count = await use_case.execute(
+        current_user,
+        order_by=ordering,
+        filters=filters.model_dump(exclude_none=True),
+        limit=params["limit"],
+        offset=params["offset"],
+    )
+    return PaginatedResponse(
+        count=count,
+        data=payments,
+        page=params["page"],
+        page_size=params["page_size"],
+    )
 
 
 @router.patch("/membership/mock", response_model=UserMembershipViewSchema)
