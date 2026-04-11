84344c74181c4da6cf10e7140165392a9bf80367
LizVin19 <e192000@gmail.com>
2026-04-12 00:01:05 +0300

    fix: ruff issues in migration

--- alembic/versions/ae959c5011a7_merge_alembic_heads.py
+++ alembic/versions/ae959c5011a7_merge_alembic_heads.py
@@ -7,10 +7,6 @@ Create Date: 2026-04-11 21:02:26.442491
 """
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
 revision: str = 'ae959c5011a7'
 down_revision: Union[str, None] = ('50ef4536f557', 'e25423541c91')
