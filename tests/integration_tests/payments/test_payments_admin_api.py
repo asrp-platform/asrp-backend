d2d48405024b80edeb24a3474be8f91d9fa3f1bf
Battarey <157142363+Battarey@users.noreply.github.com>
2026-05-27 13:35:13 +0300

    test: Integration tests have been added to verify access rights to payments.

--- tests/integration_tests/payments/test_payments_admin_api.py
+++ tests/integration_tests/payments/test_payments_admin_api.py
@@ -60,3 +60,37 @@ async def test_get_payments_admin_success(
     assert response.status_code == 200
     assert data["count"] >= 1
     assert len(data["data"]) >= 1
+
+
+async def test_get_payments_admin_forbidden_without_permission(
+    client: AsyncClient,
+    admin_auth_headers: AuthHeaders,
+) -> None:
+    response = await client.get("/api/admin/payments", headers=admin_auth_headers)
+
+    assert response.status_code == 403
+    assert response.json()["detail"] == "You do not have enough permissions to perform this action"
+
+
+async def test_get_payments_admin_success_with_only_payments_view_permission(
+    client: AsyncClient,
+    admin_auth_headers: AuthHeaders,
+    admin_user: User,
+    test_transaction_manager,
+    payment_factory,
+) -> None:
+    async with test_transaction_manager:
+        # Assigning rights to a test user to check rights
+        permission = await test_transaction_manager.permission_repository.get_first_by_kwargs(action="payments.view")
+        await test_transaction_manager.user_permission_repository.create(
+            permission_id=permission.id, user_id=admin_user.id
+        )
+
+    await payment_factory()
+
+    response = await client.get("/api/admin/payments", headers=admin_auth_headers)
+    data = response.json()
+
+    assert response.status_code == 200
+    assert data["count"] >= 1
+    assert len(data["data"]) >= 1
