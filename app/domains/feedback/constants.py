e9d4618fa8d8fd7ff45026998430c32b872deea8
Aleksei Alekseev <angelpsymail@gmail.com>
2026-06-07 16:31:18 +0000

    feat: add methods to Feedback service

---
+++ app/domains/feedback/constants.py
@@ -0,0 +1,7 @@
+HEAR_ABOUT_ASRP_OPTIONS: tuple[str, ...] = (
+    "Colleague / word of mouth",
+    "Telegram or social media",
+    "USCAP or other conference",
+    "Institutional listserv / email",
+    "Other",
+)
