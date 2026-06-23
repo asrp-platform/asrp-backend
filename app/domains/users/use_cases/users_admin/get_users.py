dff105e29cd7a465ddfe0226698f21b3605f4a2b
Maksim <maxevg72@gmail.com>
2026-06-23 17:11:38 +0300

    feat: Added avatar_url to the admin user list

--- app/domains/users/use_cases/users_admin/get_users.py
+++ app/domains/users/use_cases/users_admin/get_users.py
@@ -1,7 +1,9 @@
+import asyncio
 from typing import Annotated, Any
 
 from fastapi import Depends
 
+from app.core.storage.storage_factory import FileStorageDep
 from app.domains.shared.transaction_managers import TransactionManagerDep
 from app.domains.users.models import User
 from app.domains.users.services import UserServiceDep
@@ -12,9 +14,11 @@ class GetUsersUseCase:
         self,
         user_service: UserServiceDep,
         transaction_manager: TransactionManagerDep,
+        file_storage: FileStorageDep,
     ):
         self.__tm = transaction_manager
         self.__user_service = user_service
+        self.__file_storage = file_storage
 
     async def execute(
         self,
@@ -27,7 +31,17 @@ class GetUsersUseCase:
     ) -> [list[User], int]:
         # check_permissions("users.view", permissions)
         async with self.__tm:
-            return await self.__user_service.get_all_paginated_counted(limit, offset, order_by, filters)
+            users, count = await self.__user_service.get_all_paginated_counted(limit, offset, order_by, filters)
+
+            await asyncio.gather(*(self.__set_avatar_url(user) for user in users))
+
+            return users, count
+
+    async def __set_avatar_url(self, user: User) -> None:
+        if user.avatar_path is None:
+            return
+
+        user.avatar_url = await self.__file_storage.get_file_url(user.avatar_path)
 
 
 GetUsersUseCaseDep = Annotated[GetUsersUseCase, Depends(GetUsersUseCase)]
